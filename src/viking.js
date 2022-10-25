513ad26a7afe58465ce057b3e57a6c14f324b259
Bruno Prestes <brunoasprestes@gmail.com>
2022-10-25 08:52:49 -0300

    Solved Lab

--- src/viking.js
+++ src/viking.js
@@ -14,6 +14,10 @@ class Soldier {
   }
 }
 
+const soldado = new Soldier(100, 100);
+console.log(soldado.attack());
+console.log(soldado.receiveDamage(50));
+
 // Viking
 class Viking extends Soldier {
   constructor(name, health, strength) {
@@ -23,7 +27,7 @@ class Viking extends Soldier {
 
   receiveDamage(damage) {
     this.health -= damage;
-    if (this.health) {
+    if (this.health >= 0) {
       return `${this.name} has received ${damage} points of damage`;
     } else {
       return `${this.name} has died in act of combat`;
@@ -35,11 +39,17 @@ class Viking extends Soldier {
   }
 }
 
+const v = new Viking("Bruno", 100, 100);
+console.log(v.name, v.health, v.strength);
+console.log(v.receiveDamage(50));
+console.log(v.receiveDamage(60));
+console.log(v.battleCry());
+
 // Saxon
 class Saxon extends Soldier {
   receiveDamage(damage) {
     this.health -= damage;
-    if (this.health) {
+    if (this.health >= 0) {
       return `A Saxon has received ${damage} points of damage`;
     } else {
       return `A Saxon has died in combat`;
