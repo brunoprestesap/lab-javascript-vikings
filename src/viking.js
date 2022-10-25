15443b32f6d14d85ca06473303e9b49dc4969787
Bruno Prestes <brunoasprestes@gmail.com>
2022-10-24 23:47:53 -0300

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,52 +1,50 @@
 // Soldier
 class Soldier {
-    constructor(health, strength) {
-        this.health = health;
-        this.strength = strength;
-    }
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
 
-    attack() {
-        return this.strength;
-    }
+  attack() {
+    return this.strength;
+  }
 
-    receiveDamage(damage) {
-        this.health -= damage;
-    }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
 }
 
 // Viking
 class Viking extends Soldier {
-    constructor(name, health, strength) {
-        super(health, strength);
-        this.name = name;
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
     }
+  }
 
-    receiveDamage(damage) {
-        this.health -= damage;
-        if (this.health) {
-            return `${this.name} has received ${damage} points of damage`
-        } else {
-            return `${this.name} has died in act of combat`
-        }
-    }
-
-    battleCry() {
-        return "Odin Owns You All!";
-    }
-
-
+  battleCry() {
+    return "Odin Owns You All!";
+  }
 }
 
 // Saxon
-class Saxon extends Soldier{
-    receiveDamage(damage) {
-        this.health -= damage;
-        if (this.health) {
-            return `A Saxon has received ${damage} points of damage`
-        } else {
-            return `A Saxon has died in combat`
-        }
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
     }
+  }
 }
 
 // War
