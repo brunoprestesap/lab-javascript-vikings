99e43de3b6463dfd7620e9fd6eee292fc71b459f
Bruno Prestes <brunoasprestes@gmail.com>
2022-10-25 09:01:40 -0300

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -27,7 +27,7 @@ class Viking extends Soldier {
 
   receiveDamage(damage) {
     this.health -= damage;
-    if (this.health >= 0) {
+    if (this.health > 0) {
       return `${this.name} has received ${damage} points of damage`;
     } else {
       return `${this.name} has died in act of combat`;
@@ -49,7 +49,7 @@ console.log(v.battleCry());
 class Saxon extends Soldier {
   receiveDamage(damage) {
     this.health -= damage;
-    if (this.health >= 0) {
+    if (this.health > 0) {
       return `A Saxon has received ${damage} points of damage`;
     } else {
       return `A Saxon has died in combat`;
